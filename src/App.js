ada7b16cc7cc6112d04c793dab31833d5f5f8a41
Kuba Sukowski <kubasulek2@gmail.com>
2019-10-23 17:27:36 +0200

    jotto app set up

--- src/App.js
+++ src/App.js
@@ -2,34 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-	constructor (props) {
-		super(props);
-		this.state = {
-			counter: 0,
-			err: false
-		};
-	}
+	
 	render () {
 		return (
-			<div data-test='component-app'>
-				<h1 data-test='counter-display'>The counter is currently {this.state.counter}</h1>
-				<button
-					data-test='increment-button'
-					onClick={() => this.setState(prevState => ({
-						counter: ++prevState.counter,
-						error: false
-					}))}
-				>Increment Counter</button>
-				<button
-					data-test='decrement-button'
-					onClick={() => this.setState(prevState => ({
-						counter: prevState.counter > 0 ? --prevState.counter : prevState.counter,
-						error: prevState.counter <= 0 ? true : prevState.error
-					}))}
-				>Decrement Counter
-				</button>
-				{this.state.error ? <p data-test='error-display'>Do not decrement below zero</p> : null}
-			</div>
+			<div></div>
 		);
 
 	}
