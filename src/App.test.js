ef556ecf12ddf02e5584e02912f94bef818e6400
Kuba Sukowski <kubasulek2@gmail.com>
2019-10-23 12:33:10 +0200

    remove properties from build, using jsDocs, first tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,17 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component 
+ * @function setup 
+ * @param {object} props 
+ */
+
+const setup = (props={}, state=null) => shallow(<App {...props}/>);
+
+const wrapper = shallow(<App />);
+
 test('render without errors', () => {
-	const wrapper = shallow(<App />);
 	const appComponent = wrapper.find('[data-test="component-app"]');
 	expect(appComponent).toHaveLength(1);
 	
@@ -14,11 +23,13 @@ test('render without errors', () => {
 });
 
 test('render increment button', () => {
-	
+	const incrementButton = wrapper.find('[data-test="increment-button"]');
+	expect(incrementButton).toHaveLength(1);
 });
 
 test('counter display', () => {
-	
+	const counterDisplay = wrapper.find('[data-test="counter-display"]');
+	expect(counterDisplay).toHaveLength(1);
 });
 
 test('counter starts at 0', () => {
