9aa35b01ea6b66fe51d039cd39930b58fa795c8c
Kuba Sukowski <kubasulek2@gmail.com>
2019-10-24 13:42:55 +0200

    GuestWord first tests passed

--- src/Components/GuestWords/GuestWord.test.js
+++ src/Components/GuestWords/GuestWord.test.js
@@ -3,9 +3,9 @@ import { shallow } from 'enzyme';
 import { checkProps, findByTestAttr } from '../../test/testUtils';
 import GuestWords from './GuestWords';
 
-const defaultProps = [ {
-	guessedWords: [ { guessedWords: 'train', letterMatchCount: 3 } ]
-} ];
+const defaultProps = {
+	guessedWords: [ { guessedWord: 'train', letterMatchCount: 3 } ]
+};
 
 
 /**
@@ -22,4 +22,33 @@ const setup = (props = {}) => {
 
 test('does not throw warning with expected props', () => {
 	checkProps(GuestWords, defaultProps);
+});
+
+
+
+describe('if there are no words guessed', () => {
+	
+	let wrapper;
+	beforeEach(() => {
+		wrapper = setup({ guessedWords: [] });
+	});
+	
+	test('renders without errors', () => {
+		const component = findByTestAttr(wrapper, 'component-guessed-words');
+		expect(component).toHaveLength(1);
+
+
+	});
+	test('renders instructions to guess a word', () => {
+		const instruction = findByTestAttr(wrapper, 'guess-instructions');
+		expect(instruction.text()).not.toHaveLength(0);
+		
+	});
+});
+
+describe('if there are words guessed', () => {
+	test('renders without errors', () => {
+		const wrapper = setup();
+		expect(wrapper).toHaveLength(1);
+	});
 });
--- src/Components/GuestWords/GuestWords.js
+++ src/Components/GuestWords/GuestWords.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuestWords = (props) => {
+const GuestWords = ({guessedWords}) => {
+	let contents;
+	if(guessedWords.length === 0){
+		contents=(
+			<span data-test='guess-instructions'>
+				Try to guess the secret word!
+			</span>
+		);
+	}
 	return (
-		<div>
-			
+		<div data-test='component-guessed-words'>
+			{contents}
 		</div>
 	);
 };
-GuestWords.PropTypes = {
-
+GuestWords.propTypes = {
+	guessedWords: PropTypes.arrayOf(
+		PropTypes.shape({
+			guessedWord: PropTypes.string.isRequired,
+			letterMatchCount: PropTypes.number.isRequired
+		})
+	).isRequired
 };
 
 export default GuestWords;
