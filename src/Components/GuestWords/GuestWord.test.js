dcc8de3b736bd7bb06d71d6b8f72c9c38710ff86
Kuba Sukowski <kubasulek2@gmail.com>
2019-10-24 14:06:29 +0200

    GuestWords table tested

--- src/Components/GuestWords/GuestWord.test.js
+++ src/Components/GuestWords/GuestWord.test.js
@@ -17,7 +17,7 @@ const defaultProps = {
 
 const setup = (props = {}) => {
 	const setupProps = { ...defaultProps, ...props };
-	return shallow(<GuestWords {...setupProps}/>);
+	return shallow(<GuestWords {...setupProps} />);
 };
 
 test('does not throw warning with expected props', () => {
@@ -27,12 +27,12 @@ test('does not throw warning with expected props', () => {
 
 
 describe('if there are no words guessed', () => {
-	
+
 	let wrapper;
 	beforeEach(() => {
 		wrapper = setup({ guessedWords: [] });
 	});
-	
+
 	test('renders without errors', () => {
 		const component = findByTestAttr(wrapper, 'component-guessed-words');
 		expect(component).toHaveLength(1);
@@ -42,13 +42,34 @@ describe('if there are no words guessed', () => {
 	test('renders instructions to guess a word', () => {
 		const instruction = findByTestAttr(wrapper, 'guess-instructions');
 		expect(instruction.text()).not.toHaveLength(0);
-		
+
 	});
 });
 
 describe('if there are words guessed', () => {
-	test('renders without errors', () => {
-		const wrapper = setup();
-		expect(wrapper).toHaveLength(1);
+	let wrapper;
+	const guessedWords = [
+		{ guessedWord: 'train', letterMatchCount: 3 },
+		{ guessedWord: 'agile', letterMatchCount: 1 },
+		{ guessedWord: 'party', letterMatchCount: 5 },
+	];
+	beforeEach(() => {
+		wrapper = setup({ guessedWords });
 	});
+
+	test('renders without errors', () => {
+		const component = findByTestAttr(wrapper, 'component-guessed-words');
+		expect(component).toHaveLength(1);
+	});
+
+	test('renders table with guesses', () => {
+		const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+		expect(guessedWordsNode.length).toBe(1)
+	});
+
+	test('display correct number of guessed words', () => {
+		const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+		expect(guessedWordsNodes.length).toBe(guessedWords.length);
+	});
+
 });
--- src/Components/GuestWords/GuestWords.js
+++ src/Components/GuestWords/GuestWords.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuestWords = ({guessedWords}) => {
+const GuestWords = ({ guessedWords }) => {
 	let contents;
-	if(guessedWords.length === 0){
-		contents=(
+	if (guessedWords.length === 0) {
+		contents = (
 			<span data-test='guess-instructions'>
 				Try to guess the secret word!
 			</span>
+		);
+	} else {
+		const guessedWordsRows = guessedWords.map(({ guessedWord, letterMatchCount }) => (
+			<tr data-test='guessed-word' key={guessedWord}>
+				<td>{guessedWord}</td>
+				<td>{letterMatchCount}</td>
+			</tr>
+		));
+		contents = (
+			<div data-test='guessed-words'>
+				<h3>Guessed Words</h3>
+				<table>
+					<thead>
+						<tr><th>Guess</th><th>Matching Letters</th></tr>
+					</thead>
+					<tbody>
+						{guessedWordsRows}
+					</tbody>
+				</table>
+			</div>
 		);
 	}
 	return (
